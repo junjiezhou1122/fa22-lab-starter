4a9259c722e9c12278971ee03c0597753fd949c6
Junjie Zhou <menging1122@gmail.com>
2024-09-22 10:59:25 +0800

    finish the lab1

--- lab01/exercise3/ll_cycle.c
+++ lab01/exercise3/ll_cycle.c
@@ -1,6 +1,22 @@
-#include <stddef.h>
+#include <stdbool.h>
 #include "ll_cycle.h"
+#include <stdio.h>
+bool ll_has_cycle(node *head) {
+    if (head == NULL) {
+        return false;
+    }
 
-int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    node *slow_ptr = head;
+    node *fast_ptr = head;
+
+    while (fast_ptr != NULL && fast_ptr->next != NULL) {
+        slow_ptr = slow_ptr->next;
+        fast_ptr = fast_ptr->next->next;
+
+        if (slow_ptr == fast_ptr) {
+            return true;
+        }
+    }
+
+    return false;
 }
